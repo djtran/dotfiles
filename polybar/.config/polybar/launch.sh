4b49098cab77406ad590ec1d0ed761d7bd5739dc
davtra <davtra@amazon.com>
2019-12-13 15:00:26 -0500

    Update polybar config to what I'm using at work

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -6,8 +6,12 @@ killall -q polybar
 #Wait until processes shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-#Launch bar
-polybar bar &
-#polybar workspaces &
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload bar &
+  done
+else
+  polybar --reload bar &
+fi
 
 echo "Bar launched..."
